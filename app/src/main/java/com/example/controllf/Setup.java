9164b91b4c49435dc4902a0805dd6e98312b71bb
Samiul <samiulpias71@gmail.com>
2023-03-24 00:42:23 +0600

    F1

--- app/src/main/java/com/example/controllf/Setup.java
+++ app/src/main/java/com/example/controllf/Setup.java
@@ -1,33 +1,25 @@
 package com.example.controllf;
-import androidx.annotation.NonNull;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.lang.reflect.Method;
 import java.util.Set;
+import java.util.UUID;
 
 @SuppressWarnings("ALL")
 public class Setup extends AppCompatActivity {
@@ -37,6 +29,8 @@ public class Setup extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     BluetoothAdapter bluetoothAdapter;
 
+    private RadioGroup ltGroup, fanGroup;
+    private SharedPreferences ltGroupPrefs, fanGroupPrefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,114 +61,118 @@ public class Setup extends AppCompatActivity {
                     }
                 });
 
-        RadioGroup LtGroup = findViewById(R.id.LtGroup);
-        RadioGroup FanGroup = findViewById(R.id.FanGroup);
-
         ListView listView = findViewById(R.id.blueList);
         TextView connectbtn = findViewById(R.id.connectButton);
-        // Add a listener to the radio group
-        LtGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+        ltGroup = findViewById(R.id.LtGroup);
+        fanGroup = findViewById(R.id.FanGroup);
+
+        // Get the SharedPreferences instances
+        ltGroupPrefs = getSharedPreferences("LtGroupPrefs", MODE_PRIVATE);
+        fanGroupPrefs = getSharedPreferences("FanGroupPrefs", MODE_PRIVATE);
+
+        // Load the saved data from SharedPreferences and set the corresponding radio buttons as checked
+        int selectedButtonIdLtGroup = ltGroupPrefs.getInt("selectedButtonId", -1);
+        if (selectedButtonIdLtGroup != -1) {
+            ltGroup.check(selectedButtonIdLtGroup);
+        }
+        int selectedButtonIdFanGroup = fanGroupPrefs.getInt("selectedButtonId", -1);
+        if (selectedButtonIdFanGroup != -1) {
+            fanGroup.check(selectedButtonIdFanGroup);
+        }
+
+        // Set a listener to detect when a radio button is checked
+        RadioGroup.OnCheckedChangeListener radioGroupListener = new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton) findViewById(checkedId);
-                String selectedText = radioButton.getText().toString();
-
-                // Save the selected value to SharedPreferences
-                SharedPreferences prefs = getSharedPreferences("LtBtnGroup", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                RadioButton selectedRadioButton = findViewById(checkedId);
-                editor.putString("selected_value", selectedRadioButton.getText().toString());
-                editor.apply();
-
-            }
-        });
-        FanGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton) findViewById(checkedId);
-                String selectedText = radioButton.getText().toString();
-
-                // Save the selected value to SharedPreferences
-                SharedPreferences prefs = getSharedPreferences("FanBtnGroup", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                RadioButton selectedRadioButton = findViewById(checkedId);
-                editor.putString("selected_value", selectedRadioButton.getText().toString());
+                // Save the selected button's ID to SharedPreferences
+                SharedPreferences.Editor editor;
+                if (group == ltGroup) {
+                    editor = ltGroupPrefs.edit();
+                } else {
+                    editor = fanGroupPrefs.edit();
+                }
+                editor.putInt("selectedButtonId", checkedId);
                 editor.apply();
             }
-        });
+        };
+        ltGroup.setOnCheckedChangeListener(radioGroupListener);
+        fanGroup.setOnCheckedChangeListener(radioGroupListener);
+
+        // Loop through all child views of the RadioGroups to set their click listeners
+        for (int i = 0; i < ltGroup.getChildCount(); i++) {
+            View view = ltGroup.getChildAt(i);
+            if (view instanceof RadioButton) {
+                ((RadioButton) view).setClickable(true);
+                ((RadioButton) view).setFocusable(true);
+            }
+        }
+        for (int i = 0; i < fanGroup.getChildCount(); i++) {
+            View view = fanGroup.getChildAt(i);
+            if (view instanceof RadioButton) {
+                ((RadioButton) view).setClickable(true);
+                ((RadioButton) view).setFocusable(true);
+            }
+        }
         connectbtn.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("MissingPermission")
             @Override
-            public void onClick(View view) {
-                // TODO: Add code to get the list of available Bluetooth devices
-
-                if (Build.VERSION.SDK_INT >= 24) {
-                    requestPermissions(permissions, 80);
-                } else {
-                    //s
-                }
-
+            public void onClick(View v) {
                 BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-// Check if Bluetooth is enabled
-                if (bluetoothAdapter.isEnabled()) {
-                    // Get a list of paired (bonded) devices
-                    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-                    // Find the currently connected device (if there is one)
-                    BluetoothDevice connectedDevice = null;
-                    for (BluetoothDevice device : pairedDevices) {
-                        int state = bluetoothAdapter.getProfileConnectionState(BluetoothGatt.GATT);
-                        if (state == BluetoothGatt.STATE_CONNECTED && device.getBondState() == BluetoothDevice.BOND_BONDED) {
-                            connectedDevice = device;
-                            break;
-                        }
-                    }
-
-                    // If a connected device was found, update the TextView with its name
-                    if (connectedDevice != null) {// check if there is a connected device
-                        String deviceName = connectedDevice.getName(); // retrieve device name
-                        if (deviceName != null) {
-                            // check if device name is not null or empty
-                            connectbtn.setText("Connected device: " + deviceName);
-                        } else if (deviceName.isEmpty()) {
-                            // if device has no name
-                            for (BluetoothDevice device : pairedDevices) {
-                                String noName = device.getName();
-                                connectbtn.setText("Unknown" + noName);
-                            }
-                        } else {}
-                    } else {
-                        // if no device connected
-                        connectbtn.setText("No device connected.");
-                    }
+                if (bluetoothAdapter == null) {
+                    connectbtn.setText("Bluetooth not supported");
+                } else if (!bluetoothAdapter.isEnabled()) {
+                    connectbtn.setText("Turn On Bluetooth Connection");
                 } else {
-                    // Bluetooth is not enabled
-                    connectbtn.setText("Bluetooth is not enabled");
+                    BluetoothDevice connectedDevice = getConnectedDevice(bluetoothAdapter);
+                    if (connectedDevice == null) {
+                        connectbtn.setText("No Connected Device");
+                    } else {
+                        String deviceName = connectedDevice.getName();
+                        if (deviceName == null) {
+                            deviceName = "Unknown Named Device Connected";
+                        }
+                        connectbtn.setText("Connected to: " + deviceName);
+                    }
                 }
             }
         });
+    }
 
+    private BluetoothDevice getConnectedDevice(BluetoothAdapter bluetoothAdapter) {
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice device : pairedDevices) {
+            if (device.getBondState() == BluetoothDevice.BOND_BONDED && isDeviceConnected(device)) {
+                return device;
+            }
+        }
+        return null;
+    }
 
-
+    private boolean isDeviceConnected(BluetoothDevice device) {
+        try {
+            Method isConnectedMethod = BluetoothDevice.class.getDeclaredMethod("isConnected");
+            isConnectedMethod.setAccessible(true);
+            return (boolean) isConnectedMethod.invoke(device);
+        } catch (Exception e) {
+            // Handle any exceptions here
+        }
+        return false;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == 80)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 80) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 //yo
-            }else {
-                Toast.makeText(this,"Not Connected", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Not Connected", Toast.LENGTH_SHORT).show();
                 requestPermissions(permissions, 80);
             }
         }
     }
 
-    private boolean isConnected(BluetoothDevice device) {
-        int connectionState = bluetoothAdapter.getProfileConnectionState(BluetoothProfile.GATT);
-        return connectionState == BluetoothProfile.STATE_CONNECTED;
-    }
+
 
 }
